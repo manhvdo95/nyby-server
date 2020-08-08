dd8c6778f993346b118e60cafde6f66ea55a5e3c
manhvdo95 <manhvdo95@yahoo.com>
2020-08-08 15:45:46 -0400

    remove everything. This is a simple hello world app

--- src/main/java/com/rysk/nybyserver/configs/WebSecurityConfig.java
+++ src/main/java/com/rysk/nybyserver/configs/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
             }
         };
--- src/main/java/com/rysk/nybyserver/controllers/NybyPostController.java
+++ src/main/java/com/rysk/nybyserver/controllers/NybyPostController.java
@@ -1,42 +1,20 @@
 package com.rysk.nybyserver.controllers;
 
-import com.rysk.nybyserver.services.INybyPostService;
-import com.rysk.nybyserver.models.views.NybyPost;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/nybyPost")
 @AllArgsConstructor
 public class NybyPostController {
 
-    private INybyPostService nybyPostService;
-
-    @GetMapping("/post/{id}")
-    public ResponseEntity<NybyPost> getNybyPostById(@PathVariable Long id) {
+    @GetMapping("/test")
+    public ResponseEntity<String> getHelloWorld() {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(nybyPostService.getNybyPostById(id));
-    }
-
-    @GetMapping("/post")
-    public ResponseEntity<List<NybyPost>> getAllNybyPost() {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(nybyPostService.getAllNybyPost());
-    }
-
-    @PostMapping("/post")
-    public ResponseEntity<NybyPost> addNybyPost(@RequestPart MultipartFile file,
-                                                @RequestPart NybyPost nybyPost) {
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(nybyPostService.addNybyPost(nybyPost, file));
+                .body("Hello World");
     }
 
 }
